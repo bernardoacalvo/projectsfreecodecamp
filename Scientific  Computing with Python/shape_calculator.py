1e47b9a23fc93b01f21f68c4a9ebd4b7dd1d83d3
Bernardo Calvo <76449691+bernardoacalvo@users.noreply.github.com>
2022-03-18 16:15:46 +0000

    Add example

--- Scientific  Computing with Python/shape_calculator.py
+++ Scientific  Computing with Python/shape_calculator.py
@@ -56,4 +56,16 @@ class Square(Rectangle):
   def __str__(self):
     return MSG_SQUARE.format(self.width)
 
-  
+#Example
+rect = Rectangle(5, 10)
+print(rect.get_area())
+rect.set_width(3)
+print(rect.get_perimeter())
+print(rect)
+print(rect.get_picture())
+
+sq = Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
